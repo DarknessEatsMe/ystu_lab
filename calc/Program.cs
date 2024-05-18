f1e4a6f2cd82875a7b7b9f318e9f87857309ca67
DarknessEatsMe <DarknessDevoursMe@yandex.ru>
2024-05-18 12:50:43 +0300

    exit

--- calc/Program.cs
+++ calc/Program.cs
@@ -4,9 +4,12 @@
 	{
 		static void Main(string[] args)
 		{
+
+			Console.WriteLine("Добро пожаловать в калькулятор");
 			double a;
 			double b;
 			double res;
+			char quit;
 			char operation;
 
 			Console.WriteLine("Введите 1-е число: ");
@@ -16,39 +19,55 @@
 			Console.WriteLine("Введите операцию: ");
 			operation = Convert.ToChar(Console.ReadLine());
 
-			switch (operation)
+			while (true)
 			{
-				case '+':
-					res = a + b;
-					Console.WriteLine("Сумма = " + res);
-					break;
+				switch (operation)
+				{
+					case '+':
+						res = a + b;
+						Console.WriteLine("Сумма = " + res);
+						break;
 
-				case '-':
-					res = a - b;
-					Console.WriteLine("Разница = " + res);
-					break;
+					case '-':
+						res = a - b;
+						Console.WriteLine("Разница = " + res);
+						break;
 
-				case '*':
-					res = a * b;
-					Console.WriteLine("Произведение = " + res);
-					break;
+					case '*':
+						res = a * b;
+						Console.WriteLine("Произведение = " + res);
+						break;
 
-				case '/':
-					if(b== 0)
-					{
-						Console.WriteLine("Ошибка. Деление на 0");
-					}
-					else
-					{
-						res = a / b;
-						Console.WriteLine("Частное = " + res);
-					}
-					break;
+					case '/':
+						if(b== 0)
+						{
+							Console.WriteLine("Ошибка. Деление на 0");
+						}
+						else
+						{
+							res = a / b;
+							Console.WriteLine("Частное = " + res);
+						}
+						break;
 
-				case '^':
-					res = Math.Pow(a,b);
-					Console.WriteLine($"{a} в степени {b} = {res}");
+					case '^':
+						res = Math.Pow(a,b);
+						Console.WriteLine($"{a} в степени {b} = {res}");
+						break;
+
+				}
+				Console.WriteLine("Завершить работу программы (Y / N)?");
+				quit = Convert.ToChar(Console.ReadLine());
+				if (quit == 'y')
+				{
 					break;
+				}
+				else if (quit == 'n')
+				{
+					continue;
+				}
+
+
 
 			}
 		}
